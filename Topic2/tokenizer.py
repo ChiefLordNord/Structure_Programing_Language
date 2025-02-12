bc79c56c938f53e2d958a73d6d2fe9b1cfe04396
ChiefLordNord <achieffallo86@gmail.com>
2025-02-12 16:00:02 -0500

    Create tokenizer.py

---
+++ Topic2/tokenizer.py
@@ -0,0 +1,94 @@
+import re
+
+#Define patters for tokens
+patterns = [
+    [r"\d+", "number"],
+    [r"\+", "+"],
+    [r"\-", "-"],
+    [r"\*", "*"],
+    [r"\/", "/"],
+    [r"\(", "("],
+    [r"\)", ")"],
+    [r"\s+", "whitespace"]
+    [r".", "error"]
+]
+
+for pattern in patterns:
+    pattern[0] = re.compile(pattern[0])
+
+def tokenize(characters):
+    token = []
+    position = 0
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match
+        # (process errors)
+        token = {
+            "tag": tag,
+            "position": position,
+            "value": match.group(0)
+        }
+        if token["tag"] == "number":
+            token["value"] = int(token["value"])
+        if token["tag"] != "whitespace":
+            token.append(token)
+        position = match.end()
+    #append end-of-stream marker
+    token.append({
+        "tag": None,
+        "value": None,
+        "position": position,
+    })
+    return token
+
+def test_simple_token():
+    print("test simple token")
+    examples = "+-*/()"
+    for example in examples:
+        t = tokenize(example)[0]
+        assert t["tag"] == example
+        assert t["position"] == 0
+        assert t["value"] == example
+    
+def test_number_token():
+    print ("test number tokens")
+    for s in ["1", "11"]:
+        t = tokenize(s)[0]
+        assert len(t) == 2
+        assert t["tag"] == "number"
+        assert t["value"] == s
+
+def test_multiple_tokens():
+    print("test multiple tokens")
+    tokens = tokenize("1+2")
+    print(tokens)
+    assert tokens == [{'tag': 'number', 'position': 0, 'value': 1},
+                      {'tag': '+', 'position': 1, 'value': '+'},
+                      {'tag': 'number', 'position': 2, 'value': 2},
+                      {'tag': None, 'position': 3, 'value': None}]
+
+def test_whitespace():
+    print("test whitespace")
+    tokens = tokenize("1 + 2")
+    print(tokens)
+    assert tokens == [{'tag': 'number', 'position': 0, 'value': 1},
+                      {'tag': 'whitespace', 'position': 1, 'value': ' '},
+                      {'tag': '+', 'position': 2, 'value': '+'},
+                      {'tag': 'whitespace', 'position': 3, 'value': ' '},
+                      {'tag': 'number', 'position': 4, 'value': 2},
+                      {'tag': None, 'position': 5, 'value': None}]
+
+def test_error():
+    print("testing errors")
+    tokenize("$1+2")
+
+if __name__ == "__main__":
+
+    test_simple_token()
+    test_number_token()
+    test_multiple_tokens()
+    test_whitespace()
+    test_error()
